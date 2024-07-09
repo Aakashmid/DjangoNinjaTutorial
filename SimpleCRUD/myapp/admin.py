5e0858814c7823283bd72dca244e5ed8f8e1d4c2
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-07-09 15:18:08 +0530

    create all endpoints for CRUD operations

--- SimpleCRUD/myapp/admin.py
+++ SimpleCRUD/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Employee,Department
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['first_name','last_name','department','birthdate']
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display=['id','title']
--- SimpleCRUD/myapp/api.py
+++ SimpleCRUD/myapp/api.py
@@ -1,18 +1,59 @@
-from ninja import NinjaAPI
-
 from datetime import date
-from ninja import Schema
+from typing import List
+from ninja import NinjaAPI, Schema
+from django.shortcuts import get_object_or_404
 from .models import Employee
+
+
 api = NinjaAPI()
 
-# input schema for Employee data ( means how receving data should be like)
+# schema of data getting from client
 class EmployeeIn(Schema):
     first_name: str
     last_name: str
     department_id: int = None
     birthdate: date = None
 
+
+class EmployeeOut(Schema):
+    id: int
+    first_name: str
+    last_name: str
+    department_id: int = None
+    birthdate: date = None
+
+
 @api.post("/employees")
 def create_employee(request, payload: EmployeeIn):
     employee = Employee.objects.create(**payload.dict())
+    print(payload.dict())  # payload is data posted 
     return {"id": employee.id}
+
+
+@api.get("/employees/{employee_id}", response=EmployeeOut)
+def get_employee(request, employee_id: int):
+    employee = get_object_or_404(Employee, id=employee_id)
+    return employee
+
+
+# using List on EmployeeOut(in response) mean we are showing multiple data of EmployeeOut schema
+@api.get("/employees", response=List[EmployeeOut])  
+def list_employees(request):
+    qs = Employee.objects.all()
+    return qs
+
+
+@api.put("/employees/{employee_id}")
+def update_employee(request, employee_id: int, payload: EmployeeIn):
+    employee = get_object_or_404(Employee, id=employee_id)
+    for attr, value in payload.dict().items():
+        setattr(employee, attr, value)
+    employee.save()
+    return {"success": True}
+
+
+@api.delete("/employees/{employee_id}")
+def delete_employee(request, employee_id: int):
+    employee = get_object_or_404(Employee, id=employee_id)
+    employee.delete()
+    return {"success": True}
